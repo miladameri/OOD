158c203a184db41b6aacfcb25c283251e259b78c
Farzane <farzane193@gmail.com>
2017-08-28 15:13:07 +0430

    Frontend first commit

---
+++ templates/product.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Farzane on 08/28/2017.
+ */
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
