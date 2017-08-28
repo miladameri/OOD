726a6b4379c68a6497c130f7c7c54f7fcf6b08ff
Farzane <farzane193@gmail.com>
2017-08-28 16:59:25 +0430

    3rd commit - modal removed and other changes

--- templates/product.js
+++ templates/product.js
@@ -9,4 +9,16 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+};
+
+function get_selected_product_id() {
+    console.log("salam  "+document.getElementById("purchase_form").action);
+    var url = document.URL;
+    var splitted = url.split("/");
+    var product_id = splitted[splitted.length - 1];
+    console.log("p_id   "+product_id);
+
+    document.getElementById("purchase_form").action = "/buy/" + product_id;
+    console.log("hii  "+document.getElementById("purchase_form").action);
 }
+
