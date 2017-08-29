32c8792b4a6c75f374c1d3b165a520e0e14a0e18
Farzane <farzane193@gmail.com>
2017-08-29 11:00:24 +0430

    finishing search form + some minor improvements in product page(comments)

--- static/js/product.js
+++ static/js/product.js
@@ -2,23 +2,23 @@
  * Created by Farzane on 08/28/2017.
  */
 // Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
-
-function get_selected_product_id() {
-    console.log("salam  "+document.getElementById("purchase_form").action);
-    var url = document.URL;
-    var splitted = url.split("/");
-    var product_id = splitted[splitted.length - 1];
-    console.log("p_id   "+product_id);
-
-    document.getElementById("purchase_form").action = "/buy/" + product_id;
-    console.log("hii  "+document.getElementById("purchase_form").action);
-}
-
+// var modal = document.getElementById('id01');
+//
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// };
+//
+// function get_selected_product_id() {
+//     console.log("salam  "+document.getElementById("purchase_form").action);
+//     var url = document.URL;
+//     var splitted = url.split("/");
+//     var product_id = splitted[splitted.length - 1];
+//     console.log("p_id   "+product_id);
+//
+//     document.getElementById("purchase_form").action = "/buy/" + product_id;
+//     console.log("hii  "+document.getElementById("purchase_form").action);
+// }
+//
