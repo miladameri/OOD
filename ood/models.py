5eedf6f17d87b27317ea2b0a9698020a4326ffe8
miladameri <milad.ameri73@gmail.com>
2017-08-31 15:12:22 +0430

    models cleaning

--- ood/models.py
+++ ood/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-from django.db import models
 from django.contrib.auth.models import User as DjangoUser
-from django.utils import timezone
 import datetime
-# Create your models here.
+
+
 class MyUser(DjangoUser):
     phone_number = models.CharField(max_length=255)
     is_admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
+
 
 class Customer(MyUser):
     address = models.CharField(max_length=255)
@@ -18,10 +18,12 @@ class Customer(MyUser):
     class Meta:
         verbose_name = 'Customer'
 
+
 class Admin(MyUser):
     pass
     class Meta:
         verbose_name = 'Admin'
+
 
 class Product(models.Model):
     is_new = models.BooleanField()
@@ -31,16 +33,19 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
 class Order(models.Model):
     product = models.ForeignKey('Product')
     customer = models.ForeignKey('Customer')
     address = models.CharField(max_length=255)
     ttr = models.DateField(default=datetime.datetime.now())
 
+
 class Comment(models.Model):
     product = models.ForeignKey('Product')
     customer = models.ForeignKey('Customer')
     text = models.TextField()
+
 
 class Rating(models.Model):
     product = models.ForeignKey('Product')
